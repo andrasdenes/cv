4d91ac2b5db762ad71481a89749531db274af1f1
unknown <andresch.denes@gmail.com>
2026-01-23 00:32:36 +0100

    services

---
+++ Components/2_Organisms/Service.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace cv.Components._2_Organisms
+{
+    public partial class Service
+    {
+        [Parameter]
+        public string Icon { get; set; } = string.Empty;
+
+        [Parameter]
+        public string Name { get; set; } = string.Empty;
+
+        [Parameter]
+        public string Description { get; set; } = string.Empty;
+
+        [Parameter]
+        public bool Mirrored { get; set; } = false;
+
+        [Parameter]
+        public string ForCompanies { get; set; } = string.Empty;
+
+        [Parameter]
+        public string Anchor { get; set; } = string.Empty;
+    }
+}
